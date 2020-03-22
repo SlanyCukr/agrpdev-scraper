5205f96dbf6fb9cee0c6f8b4cd4b661f1cdf67b5
slanycukr <marek.hovadik@seznam.cz>
2020-03-22 21:16:13 +0100

    Created default endpoint pointing to all other endpoints
    api/api.py:
    Default endpoint pointing to all other endpoints
    Bug fixing -> grab_longest_words endpoint now removes duplicates before sorting (lot of same words were being returned beforehand)

--- api/api.py
+++ api/api.py
@@ -10,6 +10,15 @@ app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
 
+@app.route('/', methods=['GET'])
+def home():
+    endpoints = "<h3 align='center'>All endpoints</h3><br/><a href='http://slanycukr.hopto.org:5000/covid_analysis'>Covid_analysis</a><br/>" \
+                     "<a href='http://slanycukr.hopto.org:5000/grab_newest_articles?len=3'>Grab_newest_articles</a> (3 articles)<br/>" \
+                     "<a href='http://slanycukr.hopto.org:5000/grab_longest_words?len=3'>Grab_longest_words</a> (ignore words, that are 3 or less characters long)<br/>" \
+                     "<a href='http://slanycukr.hopto.org:5000/grab_most_common_words?len=8&word_len=5'>Grab_most_common_words</a> (8 words, ignore words, that are 5 or less characters long)<br/>"
+    return endpoints
+
+
 @app.route('/grab_newest_articles', methods=['GET'])
 def grab_newest_articles():
     articles_count = int(request.args.get("len"))
@@ -24,8 +33,8 @@ def grab_longest_words():
     articles = retrieve_all_articles()
 
     # finds all words in all paragraphs and sorts them based on their length
-    unsorted_words = find_words(articles)
-    sorted_words = sorted(unsorted_words, key=len)
+    unsorted_words = list(dict.fromkeys(find_words(articles)))
+    sorted_words = sorted(unsorted_words, key=len, reverse=True)
 
     return jsonify(sorted_words[-words_count:])
 
